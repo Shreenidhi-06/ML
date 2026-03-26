830dc9049ea497e2a9572f5526c2e9f0fded8e84
Shreenidhi-06 <shreenidhi.23cs150@sode-edu.in>
2026-03-26 14:41:54 +0530

    Fix formatting issues in linear_regression_boston function

--- lab7/7.py
+++ lab7/7.py
@@ -10,7 +10,7 @@ def linear_regression_boston():
  url="https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
  df = pd.read_csv(url)
  X = df[["rm"]].values 
- y = df["medv"].values 
+ y = df["medv"].values
  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
  random_state=42)
  model = LinearRegression()
